5ee22700bde80d0d546a099efbb636e06efeea1e
Your Name <you@example.com>
2021-03-01 00:30:48 -0300

    aula 03 completa

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,49 +1,40 @@
-import { useContext } from 'react';
-import { challengesContext } from '../contexts/ChallengesContext';
+import { useContext } from "react";
+import { challengesContext } from "../contexts/ChallengesContext";
 
+import styles from "../styles/components/ChallengeBox.module.css";
 
-import styles from '../styles/components/ChallengeBox.module.css';
+export function ChallengeBox() {
+  const { activeChallenge, resetChallenge } = useContext(challengesContext);
 
-export function ChallengeBox(){
-    const contextData = useContext(challengesContext);
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {activeChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe {activeChallenge.amount} </header>
 
-    console.log(contextData)
-    
-    const hasActiveChallenge = true;
-   
-    return (
-        <div className={styles.challengeBoxContainer}>
-            { hasActiveChallenge ? (
-                <div className={styles.challengeActive}>
-                    <header>Ganhe 400 xp </header>
-
-                    <main>
-                        <img src="icons/body.svg"/>
-                        <strong>Novo Desafio</strong>
-                        <p>Levante e de uma reboladinha.</p>
-                    </main>
-                    <footer>
-                        <button 
-                        type="button"
-                        className={styles.challengeFailedButton}
-                        >FAIL
-                        </button>
-                        <button 
-                        type="button"
-                        className={styles.challengeSucceededButton}
-                        >WIN
-                        </button>
-                    </footer>
-                </div >
-            ) : (
-                <div className={styles.challengeNotActive}>
-                <strong>De start no clico para receber um desafio</strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up"/>
-                    Avance de level completando desafios.
-                </p>
-            </div>
-            )}
+          <main>
+          <img src={`icons/${activeChallenge.type}.svg`} />
+            <strong>Novo Desafio</strong>
+            <p>{activeChallenge.description}</p>
+          </main>
+          <footer>
+            <button type="button" className={styles.challengeFailedButton} onClick={resetChallenge} >
+              FAIL
+            </button>
+            <button type="button" className={styles.challengeSucceededButton}>
+              WIN
+            </button>
+          </footer>
         </div>
-    )
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>De start no clico para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,57 +1,66 @@
-import {createContext, useState, ReactNode} from 'react';
-import challengs from '../../challenges.json';
+import { match } from "assert";
+import { createContext, useState, ReactNode } from "react";
+import challengs from "../../challenges.json";
 
-interface Challenges{
-type: 'body' | 'eye';
+interface Challenge {
+  type: "body" | "eye";
+  description: string;
+  amount: number;
 }
 
-interface ChallengesContextData{
+interface ChallengesContextData {
   level: number;
+  experienceToNextLevel:number;
   challengesCompleted: number;
   currentExperience: number;
-  activeChallenge:object;
-  levelUp:() => void;
-  startNewChallenge:() => void;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+  resetChallenge:() => void;
 }
 
-interface ChallengeProviderProps{
+interface ChallengeProviderProps {
   children: ReactNode;
 }
 
 export const challengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengeProvider({ children}: ChallengeProviderProps) {
+export function ChallengeProvider({ children }: ChallengeProviderProps) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
 
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState (0);
-    const [challengesCompleted, setChallengesCompleted] = useState (0);
- 
- const[activeChallenge, setActiveChallenge] = useState(null)
- 
-    function levelUp(){
-     
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow(( level + 1) * 4, 2)
+  
+  function levelUp() {
     setLevel(level + 1);
-
   }
-  function startNewChallenge(){
-    const randomChallengeIndex = Math.floor(Math.random() * challengs.length)
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challengs.length);
     const challenge = challengs[randomChallengeIndex];
-    
-    setActiveChallenge(challenge)
+
+    setActiveChallenge(challenge);
+  }
+  function resetChallenge(){
+    setActiveChallenge(null);
   }
 
-
-    return(
-        <challengesContext.Provider value={{ 
-          level,
-         challengesCompleted,
-          currentExperience,
-          levelUp,
-          startNewChallenge,
-          activeChallenge, 
-          }}>
-        {children}
-        
-        </challengesContext.Provider>
-    );
+  return (
+    <challengesContext.Provider
+      value={{
+        level,
+        challengesCompleted,
+        currentExperience,
+        experienceToNextLevel,
+        levelUp,
+        startNewChallenge,
+        activeChallenge,
+        resetChallenge,
+      }}
+    >
+      {children}
+    </challengesContext.Provider>
+  );
 }
